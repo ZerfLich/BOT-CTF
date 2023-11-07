40ec94c43a8b7a6c13a58aee22e3f538721d0fcb
Dionisius Lucky <zerflich@gmail.com>
2023-11-07 17:38:57 +0700

    update on command folder

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
-const convertTimeCommand = require("./convertTimeCommand.js");
-const countdownCommand = require("./countdownCommand.js");
+const convertTimeCommand = require("./command/convertTimeCommand.jss");
+const countdownCommand = require("./command/countdownCommand.js");
 const { Routes } = require("discord-api-types/v9");
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
